ca7ca5817ddd1895b27e95b126a291b9ea31ae8c
MilkiBlack <108261859+MaxiShelepov@users.noreply.github.com>
2026-02-13 23:52:38 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# Токен бота, полученный от @BotFather
+BOT_TOKEN = "8551671107:AAHe6Jsbs3G3xrEJlxgvZ3t53814-rpYcCk"
+
+# API ключ RuCaptcha
+RUCAPTCHA_API_KEY = "dcfc7131e39d1c8671156feaaedbf1f6"
+
+# Список разрешённых Telegram user ID (кто может управлять ботом)
+ALLOWED_USER_IDS = [8133517773]  # замените на свои
