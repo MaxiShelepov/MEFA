00a1ac5f26ff96c2344d94521040bae557f52348
MilkiBlack <108261859+MaxiShelepov@users.noreply.github.com>
2026-02-14 01:02:49 +0700

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# Токен бота, полученный от @BotFather
-BOT_TOKEN = "8596447755:AAFq7yCgIlOcnr2W7t1ANu1jdvX9e0fIfXs"
-
-# API ключ RuCaptcha
-RUCAPTCHA_API_KEY = "dcfc7131e39d1c8671156feaaedbf1f6"
-
-# Список разрешённых Telegram user ID (кто может управлять ботом)
-ALLOWED_USER_IDS = [8133517773]  # замените на свои
